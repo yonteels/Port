07a1033943ff1c7c0efbfbc4c813fc4ef22cca8a
Yonteels <leetony0612@gmail.com>
2024-09-08 02:16:56 -0400

    new file:   assets/arrow.png 	new file:   assets/checkmark.png 	new file:   assets/education.png 	new file:   assets/email.png 	new file:   assets/experience.png 	new file:   assets/github.png 	new file:   assets/linkedin.png     new file:   assets/profilepic.jpg 	new file:   index.html 	new file:   mediaqueries.css 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-link")
+    const icon = document.querySelector(".between-icon")
+    
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+
+/* card slider*/
